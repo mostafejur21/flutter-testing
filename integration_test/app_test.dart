74dd437f112ab57d07ef23f98ea35949eb7ef31e
Mostafejur Rahman <mostafejur.smuct.cse@gmail.com>
2025-10-12 10:20:22 +0600

    Add initial Windows runner and Flutter integration
    - Created a new Flutter project with Windows support.
    - Added CMake configuration files for building the Windows application.
    - Implemented the main application entry point and window management.
    - Included resource files for application icon and version information.
    - Set up plugin registration and Flutter engine initialization.
    - Added utility functions for command line argument handling and console management.
    - Configured project manifest for web compatibility.
    - Established a .gitignore file for Windows-specific build artifacts.

---
+++ integration_test/app_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:testing_app/main.dart';
+
+void main() {
+  group('Testing App', () {
+    testWidgets('Favorites operations test', (tester) async {
+      await tester.pumpWidget(const TestingApp());
+
+      final iconKeys = ['icon_0', 'icon_1', 'icon_2'];
+      for (var icon in iconKeys) {
+        await tester.tap(find.byKey(ValueKey(icon)));
+        await tester.pumpAndSettle(const Duration(seconds: 1));
+
+        expect(find.text('Added to favorites.'), findsOneWidget);
+      }
+      await tester.tap(find.text('Favorites'));
+      await tester.pumpAndSettle();
+
+      final removeIconKeys = ['remove_icon_0', 'remove_icon_1', 'remove_icon_2'];
+      for (var icon in removeIconKeys) {
+        await tester.tap(find.byKey(ValueKey(icon)));
+        await tester.pumpAndSettle(const Duration(seconds: 1));
+
+        expect(find.text('Removed from favorites.'), findsOneWidget);
+      }
+    });
+  });
+}
